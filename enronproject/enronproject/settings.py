707f1321079f912924d48548b54dcd3695d30b6a
Numan Tasaddaq <numantasaddaq786@gmail.com>
2025-07-21 20:19:57 +0200

    secret key removed

--- enronproject/enronproject/settings.py
+++ enronproject/enronproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-c7np$&^q$v#8c7*&d@*q#!d2--&m_14u(l*oniet1)jfk!zs$j"
+SECRET_KEY = "Key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
